417f5a8f98e302b5b37716aeb5a68282f5db036d
chait <chait@LAPTOP-F0UFID8N>
2018-04-25 18:40:21 -0500

    Minor updates

--- CourseAllocationSystem/src/edu/umkc/da/Utility.java
+++ CourseAllocationSystem/src/edu/umkc/da/Utility.java
@@ -14,11 +14,10 @@ import java.util.Map;
  * This class provides methods to read data from CSV files   
  */                                            
 public class Utility {
-	static String s = null;
-	List<Course> l = new ArrayList<>();
-static String s2=null; 
+    String s =null;
+	String s2=null; 
 	public  List<Course> loadCourses() {
-		
+		List<Course> l = new ArrayList<>();
 // create a map of course id and course object for each course
 		try (BufferedReader br = new BufferedReader(new FileReader("/Users/nikhilyanamadala/Git-projects/Design-Analysis-Algorithms/CourseAllocationSystem/src/edu/umkc/da/data/Courses.csv"))) {
 			String headerLine = br.readLine();
@@ -38,13 +37,32 @@ static String s2=null;
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		return null;
+		return l;
 	}
 
 	public List<Professor> loadProfessors() {
-		// create a map of profess id and professor object for each course
-		return null;
-	}
+		// create a list of profess id and professor object for each course
+		List<Professor> l = new ArrayList<>();
+				try (BufferedReader br = new BufferedReader(new FileReader("/Users/nikhilyanamadala/Git-projects/Design-Analysis-Algorithms/CourseAllocationSystem/src/edu/umkc/da/data/Professors.csv"))) {
+					String headerLine = br.readLine();
+					while ((s = br.readLine()) != null) {
+						String[] s1 = s.split("\\,");
+						Professor p = new Professor();
+						p.setProfessorID(Integer.parseInt(s1[0]));
+						p.setName(s1[1]);
+		                l.add(p);
+					}
+					for (Professor p : l) {
+						loadProfessorExpertise(p.getProfessorID());
+					}
+				} catch (FileNotFoundException e) {
+					System.out.println(e);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				return l;
+			}
 
 	public  Map<Topic, Double> loadCourseTopics(int courseId) {
 		Map<Topic,Double> m = new HashMap<>();
@@ -65,7 +83,7 @@ static String s2=null;
 										if(t.getTopicID()==Integer.parseInt(s3[0]))
 										 t.setTopicName(s3[1]); 
 									}
-								Double d=Double.parseDouble(s1[2])/100;
+								Double d=Double.parseDouble(s1[2]);
 								m.put(t, d);
 	}
 	}
@@ -77,25 +95,49 @@ static String s2=null;
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		for(Topic t:m.keySet()) {
-			System.out.println(t.getTopicID()+"..."+t.getTopicName()+"..."+m.get(t));
-		}
-	
-		return null;
+		return m;
 	}
 
-	public Map<Topic, Integer> loadProfessorExpertise() {
+	public Map<Topic, Integer> loadProfessorExpertise(int professorID) {
 		// Same as courseTopic
-		return null;
+		Map<Topic,Integer> m1 = new HashMap<>();
+		// create a map of topic and weight in % for each course.
+		// Hint - iterate over list of courses and perform operation for each course.
+		try (BufferedReader br1 = new BufferedReader(new FileReader("/Users/nikhilyanamadala/Git-projects/Design-Analysis-Algorithms/CourseAllocationSystem/src/edu/umkc/da/data/Expertise.csv"))) 
+		{
+						String headerLine1 = br1.readLine();
+						while ((s = br1.readLine()) != null) {
+							String[] s1 = s.split("\\,");
+							if(professorID==Integer.parseInt(s1[1])) {
+								Topic t = new Topic();
+								t.setTopicID(Integer.parseInt(s1[0]));
+								try (BufferedReader br = new BufferedReader(new FileReader("/Users/nikhilyanamadala/Git-projects/Design-Analysis-Algorithms/CourseAllocationSystem/src/edu/umkc/da/data/Topics.csv"))) 
+								{
+									String headerLine = br.readLine();
+									while ((s2 = br.readLine()) != null) {
+										String[] s3 = s2.split("\\,");
+										if(t.getTopicID()==Integer.parseInt(s3[0]))
+										 t.setTopicName(s3[1]); 
+									}
+								int d=Integer.parseInt(s1[2]);
+								m1.put(t, d);
+								}
+							}
+						}
+		
+	} catch (FileNotFoundException e) {
+		System.out.println(e);
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
 	}
-
+		for(Topic t:m1.keySet()) {
+			System.out.println(t.getTopicID()+"..."+t.getTopicName()+"..."+m1.get(t));
+		}
+		return m1;
+	}
 	public static void main(String[] args) {
-		Utility u = new Utility();
-		u.loadCourses();
-		
-		
-		
-		
+Utility u = new Utility();
+  u.loadProfessors();
 	}
-}
+	}
