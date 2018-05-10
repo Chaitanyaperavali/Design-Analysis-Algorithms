047078c47d2cfecd87b89b795f72c4c53b6e4e45
chait <chait@LAPTOP-F0UFID8N>
2018-05-09 22:15:04 -0500

    Code to save output and cost matrix in seperate files.

--- CourseAllocationSystem/src/edu/umkc/da/App.java
+++ CourseAllocationSystem/src/edu/umkc/da/App.java
@@ -1,9 +1,15 @@
 package edu.umkc.da;
 
+import java.io.IOException;
+
 //Class from where application runs
 public class App {
 	public static void main(String[] args) {
 		Mapping mapping = new Mapping();
-		mapping.getMappings();
+		try {
+			mapping.getMappings();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- CourseAllocationSystem/src/edu/umkc/da/Mapping.java
+++ CourseAllocationSystem/src/edu/umkc/da/Mapping.java
@@ -1,63 +1,83 @@
 package edu.umkc.da;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 //Allocations happen in this class.
 public class Mapping {
-	
+
 	private Course[] courses;
 	private Professor[] professors;
 	private double[][] costMatrix;
 	private int[] finalMapping;
-	
-	
+
 	public Mapping() {
 		Utility utility = new Utility();
-		
-		List<Course> crs= utility.loadCourses();
+
+		List<Course> crs = utility.loadCourses();
 		this.courses = new Course[crs.size()];
 		this.courses = crs.toArray(this.courses);
-		
+
 		List<Professor> prcs = utility.loadProfessors();
 		this.professors = new Professor[prcs.size()];
 		this.professors = prcs.toArray(this.professors);
-		
+
 		this.costMatrix = new double[this.courses.length][this.professors.length];
 	}
-	
-	//Compute Score
-	public double calculateScoreOfProfessor(Course course,Professor professor){
+
+	// Compute Score
+	public double calculateScoreOfProfessor(Course course, Professor professor) {
 		double score = 0.0;
 		for (Topic topic : professor.getTopicExpertise().keySet()) {
-			if(course.getTopics().containsKey(topic)){
-				score = score + (professor.getTopicExpertise().get(topic)/5.0) * course.getTopics().get(topic);
+			if (course.getTopics().containsKey(topic)) {
+				score = score + (professor.getTopicExpertise().get(topic) / 5.0) * course.getTopics().get(topic);
 			}
 		}
 		return score;
 	}
-	
-	
-	public void mapCourseToProfessor(){
+	/**
+	 * This method generates costmatrix*/
+	public void mapCourseToProfessor() throws IOException {
 		double[][] dupCostMatrix = new double[this.courses.length][this.professors.length];
-		for (int i = 0; i < courses.length; i++) {
-			for(int j = 0;j < professors.length;j++){
-				costMatrix[i][j] = calculateScoreOfProfessor(courses[i],professors[j]);
-				dupCostMatrix[i][j] = 1.0 - costMatrix[i][j];
-			}
-		}
 		
-		//duplicate matix where each cell is {maxPossibleValue - currentValue}
+		BufferedWriter writer = new BufferedWriter(new FileWriter(".\\cost_matrix.csv"));
+		
+		for(int i=0;i<professors.length;i++){
+			writer.write(","+professors[i].getName());
+		}
+		writer.write("\n");
+		for (int i = 0; i < courses.length; i++) {
+			writer.write(courses[i].getCourseName()+",");
+			for (int j = 0; j < professors.length; j++) {
+				costMatrix[i][j] = calculateScoreOfProfessor(courses[i], professors[j]);
+				dupCostMatrix[i][j] = 1.0 - costMatrix[i][j];
+				writer.write(costMatrix[i][j]+",");
+			}
+			writer.write("\n");
+		}
+		writer.close();
+
+		// duplicate matix where each cell is {maxPossibleValue - currentValue}
 		HungImp hungImp = new HungImp(dupCostMatrix);
-		//index - Course, value - Professor
+		// index - Course, value - Professor
 		finalMapping = hungImp.execute();
 	}
-	
-	public void getMappings(){
-		//calling this method updates final mappings.
+
+	public void getMappings() throws IOException {
+		// calling this method updates final mappings.
 		mapCourseToProfessor();
-		System.out.println("Course                Professor             Score");
-		for(int i=0;i<finalMapping.length;i++){
-			System.out.println(courses[i].getCourseName() + " -- " + professors[finalMapping[i]].getName() + " -- " + costMatrix[i][finalMapping[i]]);
+		String str = "";
+		BufferedWriter writer = new BufferedWriter(new FileWriter(".\\Output.csv"));
+		writer.write("Course,Professor,Score\n");
+		for (int i = 0; i < finalMapping.length; i++) {
+			str = courses[i].getCourseName() + "," + professors[finalMapping[i]].getName() + ","
+					+ costMatrix[i][finalMapping[i]] + "\n";
+			writer.write(str);
 		}
+		writer.close();
+
 	}
 }
